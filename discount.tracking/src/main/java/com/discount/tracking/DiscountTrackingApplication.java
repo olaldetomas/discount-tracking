38c8b2cf98c8623ad22ee41a0f4d7c8c65da58c5
Tomas <olaldetomas1@gmail.com>
2021-03-28 00:58:20 -0300

    intento https

--- discount.tracking/src/main/java/com/discount/tracking/DiscountTrackingApplication.java
+++ discount.tracking/src/main/java/com/discount/tracking/DiscountTrackingApplication.java
@@ -1,8 +1,9 @@
 package com.discount.tracking;
 
 import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,15 +14,24 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
+import javax.net.ssl.SSLException;
+
+
 @SpringBootApplication
 public class DiscountTrackingApplication {
 
+
 	@Bean
-	public WebClient getWebClient()
-	{
+	public WebClient getWebClient() throws SSLException {
 		String accessToken = "TEST-2636781233817184-081411-7301f88e48e73714f454378c557a37b3-292162037";
 
+		SslContext sslContext = SslContextBuilder
+				.forClient()
+				.trustManager(InsecureTrustManagerFactory.INSTANCE)
+				.build();
+		ClientHttpConnector httpConnector = (ClientHttpConnector) HttpClient.create().secure(t -> t.sslContext(sslContext) );
 		return WebClient.builder()
+				.clientConnector(httpConnector)
 				.baseUrl("https://api.mercadopago.com/")
 				.defaultHeader(
 						HttpHeaders.CONTENT_TYPE,
--- discount.tracking/src/main/java/com/discount/tracking/controllers/PaymentController.java
+++ discount.tracking/src/main/java/com/discount/tracking/controllers/PaymentController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient.UriSpec;
 
 @RestController
 @RequestMapping(value = "payment")
@@ -21,7 +19,7 @@ public class PaymentController {
         // https://api.mercadopago.com/payments/search?sort=date_created&criteria=desc
 
         String response = webClient.get()
-                .uri("payments/search?sort=date_created&criteria=desc")
+                .uri("v1/identification_types")
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
